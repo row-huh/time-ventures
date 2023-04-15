16cc9041a079c3872f40d0ec50a36910f704d8bf
Roha <rohaabropathan@gmail.com>
2023-04-15 13:11:36 +0500

    Tightened up the code
    The code in play.py was relatively short, so it was combined with main.py.

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-
-# day 3 of building rpg
-
 import os
 import rpgfunc
-import play
+import phase1_choices
 
-
-# The mainest of main functions
-def mainmenu():
+# main menu
+def main():
     while True:
         
         os.system('cls')
@@ -16,7 +12,7 @@ def mainmenu():
         main_choice = input("Enter the corresponding number to:\n[1] Play\n[2] Credits\n[X] Exit\n\n-----> ").lower()
         
         if main_choice == '1':
-            play.play_menu()
+            play_menu()
             continue
             
         elif main_choice == '2':
@@ -30,6 +26,53 @@ def mainmenu():
             os.system('cls')
             print("Invalid Input")
             continue
+
+
+def play_menu():
+    while True:
+        os.system('cls')
+        rpgfunc.header()
+        global user_name
+        user_name = input("Enter your name: ").title()
+
+        if len(user_name) >= 2:
+            break
+        else:
+            input("Username must contain atleast 2 characters\n\nPress Enter to continue....")
+    phase_1(user_name)
+
+
+
+def phase_1(user_name):
+    rpgfunc.start_dialogues()    
+    while True:
+        os.system('cls')
+        rpgfunc.header()
+        print("_" * 44)
+        user_choice = input("What would you do\n[1] Scream for help\n[2] look in the room for clues\n[3] Grab your phone to try to call for help\n\n---> Enter: ")
         
-mainmenu()
+        if user_choice == "1":
+            rpgfunc.screamed_for_help()
+            continue
+        elif user_choice == "2":
+            rpgfunc.looking_for_clues()
+            continue
+        elif user_choice == "3":
+            if phase1_choices.call(user_name) == False:
+                continue
+            # a = phase1_choices.call(user_name)
+            rpgfunc.big_guy_incoming()
+            phase1_choices.first_fight()
+            rpgfunc.end()
+            print("-----> Thank You, " + user_name + " for Playing! ")
+            input('\n\nPress Enter to continue...')
+            rpgfunc.credits()
+            break
+        else:
+            print("Invalid input")
+            continue
+
+
+if __name__ == "__main__":        
+    main()
 
